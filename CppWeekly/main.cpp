e0355d58d610b74c4892812c785d82cae2b0229d
Emily Durie <edurie@gatech.edu>
2023-10-17 20:08:40 -0600

    CppWeekly Added

---
+++ CppWeekly/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+//LESSON 1: https://www.youtube.com/watch?v=v_C9BC-bXcw&list=PLs3KjaCtOwSY34fFKyhOFovFlB7LikDwe
+int main()
+{
+    std::cout <<"Hello World\n";
+    return 0;
+}
